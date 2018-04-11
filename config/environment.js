b008aa6b90f48a082693a9064724a37f327a50d0
Chris Manson <chris@stonecircle.io>
2018-04-11 23:44:03 +0100

    bump

--- config/environment.js
+++ config/environment.js
@@ -49,7 +49,6 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-
     ENV.blog.host = 'http://localhost:4200';
   }
 
